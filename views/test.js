3c5558f7504bfdbd4e4880296f2f90f5d72ea748
Mohammed Naji <moha.buy@gmail.com>
2017-03-30 11:52:54 +0300

    adding Select libarary

--- views/test.js
+++ views/test.js
@@ -3,26 +3,38 @@ import React from 'react'
 import  * as utils from '../utils.js';
 import Select from 'react-select';
 
-utils.country(function(err,data){
-  const body =JSON.parse(data.body);
-  const con =body.countries;
-  var options = con.map(function(obj) {
-     var rObj = {value: "",
-     label:""
+function getoption(cb){
 
-     };
-     rObj.value = con[obj-1];
-     rObj.label = con[obj-1];
-     return rObj;
-  });
+  utils.country(function(err,data){
+    const body =JSON.parse(data.body);
+    const con =body.countries;
+    var options = con.map(function(obj) {
+       var rObj = {value: "",
+       label:""
+
+       };
+       rObj.value = con[obj-1];
+       rObj.label = con[obj-1];
+       return rObj;
+    });
+    
 
 
+cb(options)
+  })
+
+}
+getoption(function(options){
+  function logChange(val) {
+    console.log("Selected: " + val);
+  }
 render (<Select
-  name="form-field-name"
+  name="selectcontry"
   value="one"
   options={options}
+  onChange={logChange}
+
 />
 
 ,document.getElementById('select-container'))
-
 })
